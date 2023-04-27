90e2b066b36a0c5bfa20377727b91920f62024a4
FoliFF <olof@hisham.se>
2023-04-28 00:43:19 +0200

    Rock Papper Scissor complete

--- game.js
+++ game.js
@@ -46,30 +46,60 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  
-  for (let i = 1; i <= 5; i++) {
-    let playerSelection = prompt(`Round ${i}: Choose Rock, Paper, or Scissors`);
-    let computerSelection = computerPlay();
-    
-    let roundResult = playRound(playerSelection, computerSelection);
-    console.log(`Round ${i}: ${roundResult}`);
-    
-    if (roundResult.startsWith("You Win!")) {
-      playerScore++;
-    } else if (roundResult.startsWith("You Lose!")) {
-      computerScore++;
-    }
+  let scores = { player: 0, computer: 0 };
+
+  const rockButton = document.querySelector('#rock');
+  const paperButton = document.querySelector('#paper');
+  const scissorsButton = document.querySelector('#scissors');
+
+  rockButton.addEventListener('click', function() {
+    const roundResult = playRound('rock', computerPlay());
+    displayResult(roundResult);
+    updateScore(roundResult, scores);
+  });
+
+  paperButton.addEventListener('click', function() {
+    const roundResult = playRound('paper', computerPlay());
+    displayResult(roundResult);
+    updateScore(roundResult, scores);
+  });
+
+  scissorsButton.addEventListener('click', function() {
+    const roundResult = playRound('scissors', computerPlay());
+    displayResult(roundResult);
+    updateScore(roundResult, scores);
+  });
+}
+
+function displayResult(result) {
+  const resultsDiv = document.querySelector('#results');
+  const p = document.createElement('p');
+  p.textContent = result;
+  resultsDiv.appendChild(p);
+}
+
+function updateScore(result, scores) {
+  const scoreDiv = document.querySelector('#score');
+  if (result.startsWith('You Win!')) {
+    scores.player++;
+  } else if (result.startsWith('You Lose!')) {
+    scores.computer++;
   }
-  
-  if (playerScore > computerScore) {
-    console.log(`You win the game! Final score: ${playerScore} to ${computerScore}`);
-  } else if (playerScore < computerScore) {
-    console.log(`You lose the game! Final score: ${playerScore} to ${computerScore}`);
-  } else {
-    console.log(`It's a tie! Final score: ${playerScore} to ${computerScore}`);
+  scoreDiv.textContent = `Player: ${scores.player} - Computer: ${scores.computer}`;
+  if (scores.player === 5) {
+    displayResult('You win the game!');
+    disableButtons();
+  } else if (scores.computer === 5) {
+    displayResult('You lose the game!');
+    disableButtons();
   }
+}
+
+function disableButtons() {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(button => {
+    button.disabled = true;
+  });
 }
 
 // Call the game function to start the game
